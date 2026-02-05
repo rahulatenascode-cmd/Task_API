dea3105eaaf4648537d599c5c046be5dc7a01b53
rahulatenascode-cmd <rahul.atenascode@gmail.com>
2026-02-05 16:13:55 +0530

    Update deps.py

--- app/core/deps.py
+++ app/core/deps.py
@@ -12,7 +12,6 @@ from app.core.security import SECRET_KEY, ALGORITHM
 
 security = HTTPBearer(auto_error=True)
 
-# Dependency: Get the current logged-in user from JWT
 def get_current_user(
     credentials: HTTPAuthorizationCredentials = Depends(security),
     session: Session = Depends(get_session)
@@ -32,7 +31,6 @@ def get_current_user(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return user
 
-# Dependency: Require a specific permission
 def require_permission(permission_name: str):
     def checker(
         user: User = Depends(get_current_user),
@@ -50,3 +48,4 @@ def require_permission(permission_name: str):
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden")
         return user
     return checker
+
